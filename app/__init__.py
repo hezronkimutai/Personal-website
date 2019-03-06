636a73ce1d139f87213aa58eb78e35bb844784d5
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-03-06 10:44:54 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import os
+import json
 from flask_restful import Api, Resource, reqparse
 import psycopg2
-from flask import Flask
+from flask_cors import CORS
+from flask import Flask, request
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
@@ -16,13 +18,21 @@ def create_app(name_conf):
     app.secret_key = os.getenv('SECRET_KEY')
     db_url = app_config[name_conf].Database_Url
     conn = psycopg2.connect(db_url)
-    return app,conn
+    CORS(app, origins="http://localhost:3000", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+    supports_credentials=True, intercept_exceptions=False)
+    api = Api(app, catch_all_404s=True)
+    return app,conn, api
 
 
 # testapp, conn = create_app(os.getenv('App_Test'))
-app, conn = create_app(os.getenv('FLASK_ENV'))
+app, conn, api = create_app(os.getenv('FLASK_ENV'))
+
+
+
+# data = json.load(open('dictionary.data.json'))
 # testapi = Api(testapp, catch_all_404s=True)
-api = Api(app, catch_all_404s=True)
+
 # testjwt = JWTManager(testapp)
 jwt = JWTManager(app)
 cursor = conn.cursor()
@@ -39,25 +49,43 @@ article_parser.add_argument('body',
                             help='Please provide the body of the article',
                             required=True)
 
+cursor.execute("""CREATE TABLE IF NOT EXISTS articles
+           (article_id SERIAL PRIMARY KEY,
+           title varchar,
+           body varchar);""")
+
+
+def getarticles():
+    cursor.execute("""SELECT * FROM articles;""")
+    rows = [x for x in cursor]
+    cols = [x[0] for x in cursor.description]
+    articles = []
+    for row in rows:
+      article = {}
+      for prop, val in zip(cols, row):
+        article[prop] = val
+      articles.append(article)
+    return articles
+
 
 def createarticle(title, body):
     try:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS articles
-                   (article_id SERIAL PRIMARY KEY,
-                   title varchar,
-                   body varchar);""")
+
         cursor.execute("""SELECT * FROM articles;""")
         cursor.execute("""INSERT INTO articles
                        (title, body)
                        VALUES('{}', '{}');""".format(title, body))
         conn.commit()
         return {'status': 201,
-                'message': 'the user was succesfully created'}, 201
+                'message': 'the article was succesfully created'}, 201
     except Exception as e:
         print(e)
 
 
 class Articles(Resource):
+    def get(self):
+        return getarticles()
+
     def post(self):
         args = article_parser.parse_args()
         title = args['title']
